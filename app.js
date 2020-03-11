3b3ebf3103f23d8bbe320162a5f5c60f3a31e8d1
Vivian Phu <viviana.phu@gmail.com>
2020-03-10 19:52:43 -0700

    if u put a letter, itll display the red error msg

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const num1 = prompt('please enter a number between 5 and 20: ')
 const output = document.getElementById('output')
 
 
-if (num1<5 || num1>20){ // this ensures that num1 will be between 5 and 20.
+if (num1<5 || num1>20 || isNaN(num1)==true){ // this ensures that num1 will be between 5 and 20.
     var printResult = "the valid input is a number between 5 and 20, please refresh your page to start again." //if it is not in the boundary, display this msg
     output.innerHTML = printResult; //prints this into the <h2> from the index.html
 
 }
 var table = `<table border="1"><tbody>`;
-
 
 for(var i = 0 ; i <= num1 ;i++){
     table += "<tr>"
