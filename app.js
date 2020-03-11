b80b5dc81f619cebace4ef99c6c68416f8cf3592
Vivian Phu <viviana.phu@gmail.com>
2020-03-10 19:17:19 -0700

    set boundaries

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // use your hs knowledge to calculte prime numbers
 
-const num1 = prompt('please enter a number: ')
-
-const doubled = num1 * 2
+const num1 = prompt('please enter a number between 5 and 20: ')
 
 const output = document.getElementById('output')
 
-const outputStr = `The doubled is ${doubled}`
 
-output.innerHTML = '<h2>' + outputStr + '</h2>'
+if (num1<5 || num1>20){ // this ensures that num1 will be between 5 and 20.
+    var printResult = "the valid input is a number between 5 and 20, please refresh your page to start again." //if it is not in the boundary, display this msg
+    output.innerHTML = printResult; //prints this into the <h2> from the index.html
+
+}
+
+
+
+
+
