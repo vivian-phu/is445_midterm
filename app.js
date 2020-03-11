fbfa348eaf2b606d6d7c2f133742a16d27742152
Vivian Phu <viviana.phu@gmail.com>
2020-03-10 19:08:27 -0700

    skeleton

---
+++ app.js
@@ -0,0 +1,11 @@
+// use your hs knowledge to calculte prime numbers
+
+const num1 = prompt('please enter a number: ')
+
+const doubled = num1 * 2
+
+const output = document.getElementById('output')
+
+const outputStr = `The doubled is ${doubled}`
+
+output.innerHTML = '<h2>' + outputStr + '</h2>'
